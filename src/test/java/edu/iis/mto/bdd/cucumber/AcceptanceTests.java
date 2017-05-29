edd264864c6b5a9ade7e1052e49a992486b08939
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-29 14:00:31 +0200

    geckodriver property refactorization

--- src/test/java/edu/iis/mto/bdd/cucumber/AcceptanceTests.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/AcceptanceTests.java
@@ -4,9 +4,12 @@ import org.junit.BeforeClass;
 
 public class AcceptanceTests {
 
+    private static final String GECKODRIVER_NAME = "geckodriver.exe";
+    private static final String GECKO_PATH = "path\\to\\";
+
     @BeforeClass
     public static void init() {
-        System.setProperty("webdriver.gecko.driver", "path\\to\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", GECKO_PATH + GECKODRIVER_NAME);
 
     }
 }
